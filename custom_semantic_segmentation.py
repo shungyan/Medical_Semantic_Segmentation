d66b8ec72b8738c357ef90b481f7fb78d5cbbb15
shungyan <shungyan@hotmail.my>
2023-02-10 23:29:59 +0800

    edit custom semantic segmentation

--- custom_semantic_segmentation.py
+++ custom_semantic_segmentation.py
@@ -4,11 +4,13 @@ from PIL import Image
 
 from pycoral.adapters import common
 from pycoral.adapters import segment
-from pycoral.utils.edgetpu import make_interpreter
+# from pycoral.utils.edgetpu import make_interpreter
+import tflite_runtime.interpreter as tflite
 
 def main():
     # Load the TFLite model
-    interpreter = make_interpreter("test_quant.tflite", device=':0')
+    # interpreter = make_interpreter("test_quant.tflite", device=':0')
+    interpreter = tflite.Interpreter(model_path="model.tflite")
 
 if __name__ == '__main__':
   main()
